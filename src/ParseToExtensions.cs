cb498822b696aa5b6a218c0576ae0c200f271859
Sam Blundell <samblundell1990@googlemail.com>
2019-10-30 13:24:09 +0000

    ParseTo<T> Comment

--- src/ParseToExtensions.cs
+++ src/ParseToExtensions.cs
@@ -3,8 +3,15 @@ namespace ParseTo.Extensions
 {
     public static class ParseToExtensions
     {
-        private static Handler handler { get; set; } = new DefaultParseToHandler();
+        public static Handler handler { get; set; } = new DefaultParseToHandler();
         
+        /// <summary>
+        /// Parses an object to either the type of T or a default
+        /// </summary>
+        /// <typeparam name="T">The type you wish to cast to</typeparam>
+        /// <param name="i">The object you wish to cast</param>
+        /// <param name="defaultValue">Custom default to override the base default</param>
+        /// <returns></returns>
         public static T ParseTo<T>(this object i, T defaultValue = default)
         {
             return handler.Handle<T>(i, defaultValue);
